dbb68285bad167f1ac6c6305bdd15f4efab7e98b
Tim <tim@tim-bowman.com>
2017-08-25 13:42:41 +1000

    Added about section, started services, added on scroll functions

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,8 @@
 // vars
-var wrapper = document.getElementsByClassName('wrapper')[0];
+var wrapper = document.getElementsByClassName('wrapper');
 var hero = document.getElementById('hero').getElementsByTagName('h1')[0];
+var header = document.getElementById('header');
+var aboutspan = document.getElementById('about').getElementsByTagName('span')[0];
 
 
 // functions
@@ -10,31 +12,77 @@ function fadeIn(e) {
 function fadeOut(e) {
 	e.style.opacity = 0;
 }
-var text = ['Front-End Developer', 'Sydney', 'Tim Bowman'];
+function addClass(e, c) {
+	e.classList.add(c);
+}
+function removeClass(e, c) {
+	e.classList.remove(c);
+}
+// hero sequence
+var heroTxt = ['Front-End Developer', 'Sydney', 'Tim Bowman'];
 
-        textSequence(0);
-        function textSequence(i) {
+        heroSequence(0);
+        function heroSequence(i) {
 
-            if (text.length > i) {
+            if (heroTxt.length > i) {
                 setTimeout(function(){
 									fadeOut(hero);
 								}, 2000);
 								setTimeout(function(){
-									hero.textContent = text[i];
+									hero.textContent = heroTxt[i];
 								}, 3000);
 								setTimeout(function(){
 									fadeIn(hero);
-									textSequence(++i);
+									heroSequence(++i);
 								}, 3000);
 
-            } else if (text.length == i) { // Loop
-                textSequence(0);
+            } else if (heroTxt.length == i) { // Loop
+                heroSequence(0);
             }
 
         }
 
+				// about sequence
+
+				var aboutTxt = ['Emails', 'Websites', 'Landing Pages', 'Innovation', 'Coding'];
+
+				        aboutSequence(0);
+				        function aboutSequence(i) {
+
+				            if (aboutTxt.length > i) {
+
+												setTimeout(function(){
+													aboutspan.textContent = aboutTxt[i];
+													aboutSequence(++i);
+												}, 2000);
+
+
+				            } else if (aboutTxt.length == i) { // Loop
+				                aboutSequence(0);
+				            }
+
+				        }
+
 
 // events
 window.addEventListener('load', function() {
-	fadeIn(wrapper);
+	fadeIn(wrapper[0]);
+});
+
+window.addEventListener('scroll', function() {
+	if(window.pageYOffset > 0) {
+		addClass(header, 'active');
+	} else {
+		removeClass(header, 'active');
+	}
+	if(window.pageYOffset > 15) {
+		fadeIn(wrapper[1]);
+	} else {
+		fadeOut(wrapper[1]);
+	}
+	if(window.pageYOffset > 340) {
+		fadeIn(wrapper[2]);
+	} else {
+		fadeOut(wrapper[2]);
+	}
 });
